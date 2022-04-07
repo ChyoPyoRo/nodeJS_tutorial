a73ceb2ccd1697b3ed2ff0005d179b333da8386a
ChyoPyoRo <allen15590@naver.com>
2022-04-07 20:00:12 +0900

    ~49

---
+++ syntax/object.js
@@ -0,0 +1,18 @@
+var members = ['egoin','dsjakslf','fjdklafj']
+console.log(members[1])
+//dsjakslf 
+
+var roles={
+    'programmer': 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'jkjk'
+}
+console.log(manager)
+
+
+var i =0;
+while(i<members.length){
+    console.log(members[i])
+    i=i+1
+}
+
